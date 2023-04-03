7ee71b07fa5ac9ec44b562168b50a29d9837393b
Nathan Ahn <nathanahn03@gmail.com>
2023-04-02 23:16:16 -0400

    dept ratio and nation code combine by 2021

--- visualizations/project_data/templates/app.js
+++ visualizations/project_data/templates/app.js
@@ -98,7 +98,7 @@
 
 
 // });
-// --------------------------------
+//--------------------------------
 // select the dropdown menu element
 // const select = d3.select('#country-select');
 
@@ -125,28 +125,196 @@
 // });
 // ---------------
 
-function init() {
-  // Get a reference to the dropdown menu
-  let dropdownMenu = d3.select("#country-select");
+// function init() {
+//   // Get a reference to the dropdown menu
+//   let dropdownMenu = d3.select("#country-select");
 
-    // Load the data from the JSON file
-  d3.json("sample.json").then(data => {
-    console.log(data);
-    // Get the country names from the data
-    let countryNames = data.map(d => d.Countries);
-    console.log(countryNames);
+//     // Load the data from the JSON file
+//   d3.json("sample.json").then(data => {
+//     console.log(data);
+//     // Get the country names from the data
+//     let countryNames = data.map(d => d.Countries);
+//     console.log(countryNames);
 
-  // Add options to the select element for each country
-  d3.select("#country-select")
-    .selectAll("option")
-    .data(countryNames)
-    .enter()
-    .append("option")
-    .text(d => d)
-    .attr("value", d => d);
+//   // Add options to the select element for each country
+//   d3.select("#country-select")
+//     .selectAll("option")
+//     .data(countryNames)
+//     .enter()
+//     .append("option")
+//     .text(d => d)
+//     .attr("value", d => d);
 
-  createPlots(countryNames[0], data);
+//   createPlots(countryNames[0], data);
 
+//   });
+// }  
+//------------------------------------------
+// // 1. Load the data
+// function init() {
+//   d3.json("sample.json").then(function(data) {
+
+//     // 2. Create the dropdown menu
+//     var dropdown = d3.select("#country-select");
+//     var countries = data.map(function(d) { return d.Countries; });
+//     countries.forEach(function(country) {
+//       dropdown.append("option").text(country);
+//     });
+  
+  
+//     // 3. Use the selected country to filter the data
+//     dropdown.on("change", function() {
+//       var selectedCountry = d3.select(this).property("value");
+//       var filteredData = data.filter(function(d) {
+//         return d.Countries === selectedCountry;
+//       });
+//       createPlots(countries[0], data);
+//     });
+//   } 
+
+//   // Create a function to create chart
+// function createPlots(sample, data) {
+//   let sampleData = data.samples.filter(d => d.id === sample)[0];
+
+//   // Create bar chart
+//     // Create the trace for the bar chart
+//   let barDataTrace = [{
+//     // Get the top 10 OTUs for the selected sample
+//     x: sampleData.sample_values.slice(0, 10).reverse(),
+//     y: sampleData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+//     text: sampleData.otu_labels.slice(0, 10).reverse(),
+//     type: "bar",
+//     orientation: "h"
+//   }];
+//     // Define the layout for the bar chart
+//   let barLayout = {
+//     title: `Top 10 OTUs ${sample}`,
+//     xaxis: { title: "Sample Values" },
+//     yaxis: { title: "OTU IDs" }
+//   };
+
+//   Plotly.newPlot("bar", barDataTrace, barLayout);
+
+//   // Create bubble chart
+//     // Create the trace for the bubble chart
+//   let bubbleDataTrace = [{
+//     x: sampleData.otu_ids,
+//     y: sampleData.sample_values,
+//     text: sampleData.otu_labels,
+//     mode: "markers",
+//     marker: {
+//       size: sampleData.sample_values,
+//       color: sampleData.otu_ids,
+//       colorscale: "Earth"
+//     }
+//   }];
+//     // Define the layout for the bubble chart
+//   let bubbleLayout = {
+//     xaxis: { title: "OTU IDs" },
+//     yaxis: { title: "Sample Values" }
+//   };
+
+//   Plotly.newPlot("bubble", bubbleDataTrace, bubbleLayout);
+
+
+
+//   // Display metadata
+//     // Retrieve all metadata and filtering
+//   let metaData = data.metadata.filter(d => d.id == sample)[0];
+//     // Clear out any previously displayed metadata information
+//   let metaDataPanel = d3.select("#sample-metadata");
+//   metaDataPanel.html("");
+//     // Returns an array of a given object's own enumerable string-keyed property [key, value] pairs to the panel
+//   Object.entries(metaData).forEach(([key,value]) => {
+//     metaDataPanel.append("h6").text(`${key}: ${value}`);
+//   });
+
+//     // Create gauge chart
+//   let wfreq = metaData.wfreq;
+//   let guageTrace = [
+//       {
+//           domain: { x: [0, 1], y: [0, 1] },
+//           value: parseFloat(wfreq),
+//           title: {
+//             text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+//             font: {color: "black", size: 24}
+//             },
+//           type: "indicator",
+//           mode: "gauge+number",
+//           gauge: { axis: { range: [null, 9] },
+//                    steps: [
+//                       { range: [0, 2], color: "#FEF9E7" },
+//                       { range: [2, 4], color: "#FCF3CF" },
+//                       { range: [4, 6], color: "#F9E79F" },
+//                       { range: [6, 8], color: "#F7DC6F" },
+//                       { range: [8, 9], color: "#F4D03F" }
+//                     ]
+
+//                   }
+//       }
+//     ];
+//   let guageLayout = { 
+//             width: 700, 
+//             height: 600, 
+//             margin: { t: 0, b: 0 } 
+//     };
+//   Plotly.newPlot("gauge", guageTrace, guageLayout);
+// }
+
+// // Create function that updates dashboard when sample is changed
+// function optionChanged(value) {
+//   d3.json(url).then(data => {
+//     createPlots(value, data);
+//   });
+// }
+
+// // Call the initial function
+// init();
+//-----------------------
+// Load the GeoJSON file and Private debt, loans and debt securities data
+// Load the GeoJSON file
+// Initialize the map
+var map = L.map('map').setView([0, 0], 2);
+
+// Add the base tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Load the debt data
+d3.json("sample.json").then(function(data) {
+  var debtData = {};
+  data.forEach(function(d) {
+    if (d['2021'] !== "no data") {
+      debtData[d['Countries']] = parseFloat(d['2021']);
+    }
+    
   });
-}  
 
+  // Load the GeoJSON data and add it to the map
+  d3.json("countries.geojson").then(function(data) {
+    var countries = L.geoJson(data, {
+      style: function(feature) {
+        var country = feature.properties.ADMIN;
+        var debtRatio = debtData[country];
+        
+        if (debtRatio) {
+          if (debtRatio < 30) {
+            return { color: 'green' };
+          } else if (debtRatio < 60) {
+            return { color: 'orange' };
+          } else {
+            return { color: 'red' };
+          }
+        } else {
+          return { color: 'gray' };
+        }
+      }
+    });
+    console.log(typeof countries);
+    console.log(countries);
+    console.log(debtData);
+    countries.addTo(map);
+  });
+});
